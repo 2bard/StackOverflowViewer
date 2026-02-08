23e32d3968a5f09c0769647063463c024054ecef
2bard <lewisandrewbaker@gmail.com>
2026-02-08 16:00:41 +0000

    Modify LocalDataStore - return followedEvent

--- app/src/main/java/com/twobard/stackoverflowviewer/data/local/LocalDataStore.kt
+++ app/src/main/java/com/twobard/stackoverflowviewer/data/local/LocalDataStore.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
+import com.twobard.stackoverflowviewer.domain.events.FollowedEvent
 
 import com.twobard.stackoverflowviewer.domain.local.LocalDataStoreInterface
 import com.twobard.stackoverflowviewer.domain.user.User
+import com.twobard.stackoverflowviewer.ui.viewmodel.UsersListViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -25,17 +27,24 @@ class LocalDataStore(val context: Context) : LocalDataStoreInterface {
         preferences[USERS_KEY]?.mapNotNull { it.toIntOrNull() } ?: emptyList()
     }
 
-    override suspend fun changeFollowStatus(user: User) {
+    override suspend fun changeFollowStatus(user: User) : FollowedEvent? {
+
+        var followedEvent: FollowedEvent? = null
         dataStore.edit { preferences ->
             val current = preferences[USERS_KEY]?.toMutableSet() ?: mutableSetOf()
 
+
             if(current.contains(user.id.toString())){
                 current.remove(user.id.toString())
+                followedEvent = FollowedEvent.Unfollowed(user = user)
             } else {
                 current.add(user.id.toString())
+                followedEvent = FollowedEvent.Followed(user = user)
             }
 
             preferences[USERS_KEY] = current
         }
+
+        return followedEvent
     }
 }
--- app/src/main/java/com/twobard/stackoverflowviewer/domain/local/LocalDataStoreInterface.kt
+++ app/src/main/java/com/twobard/stackoverflowviewer/domain/local/LocalDataStoreInterface.kt
@@ -1,9 +1,10 @@
 package com.twobard.stackoverflowviewer.domain.local
 
+import com.twobard.stackoverflowviewer.domain.events.FollowedEvent
 import com.twobard.stackoverflowviewer.domain.user.User
 import kotlinx.coroutines.flow.Flow
 
 interface LocalDataStoreInterface {
     fun followedUsers(): Flow<List<Int>>
-    suspend fun changeFollowStatus(user: User)
+    suspend fun changeFollowStatus(user: User) : FollowedEvent?
 }
