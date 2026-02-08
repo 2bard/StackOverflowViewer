b0faabb06149e7c9cdcd3ae48169cf704ceae942
2bard <lewisandrewbaker@gmail.com>
2026-02-08 19:02:49 +0000

    Add paparazzi output

---
+++ app/build/reports/paparazzi/debug/runs/20260208184130_313722.js
@@ -0,0 +1,23 @@
+window.runs["20260208184130_313722"] = [
+  {
+    "name": "UsersList_ExtraLargeText_{1",
+    "testName": "com.twobard.stackoverflowviewer.snapshot.UserListExtraLargeTextSnapshotTest#userListExtraLargeTextScreenPreview",
+    "timestamp": "2026-02-08T18:41:30.373Z",
+    "tags": [],
+    "file": "images/4eb913527db8ab7b8ed7630f6e8ed926eb64ea75.png"
+  },
+  {
+    "name": "UsersList_ExtraLargeText_{-1694854130",
+    "testName": "com.twobard.stackoverflowviewer.snapshot.UserListExtraLargeTextSnapshotTest#userListExtraLargeTextScreenPreview",
+    "timestamp": "2026-02-08T18:41:30.460Z",
+    "tags": [],
+    "file": "images/c5ee84b780d3af5ec44abaa0c7124bf04f09ce95.png"
+  },
+  {
+    "name": "UsersList_ExtraLargeText_{-1751408501",
+    "testName": "com.twobard.stackoverflowviewer.snapshot.UserListExtraLargeTextSnapshotTest#userListExtraLargeTextScreenPreview",
+    "timestamp": "2026-02-08T18:41:30.649Z",
+    "tags": [],
+    "file": "images/21d22a4d17b4184f3950ecfae7faa5ceef61259d.png"
+  }
+];
---
+++ app/build/reports/paparazzi/debug/runs/20260208184131_e9aecc.js
@@ -0,0 +1,9 @@
+window.runs["20260208184131_e9aecc"] = [
+  {
+    "name": "UsersList_IsLoading",
+    "testName": "com.twobard.stackoverflowviewer.snapshot.UserListSnapshotTest#userListLargeTextLoadingScreenPreview",
+    "timestamp": "2026-02-08T18:41:31.400Z",
+    "tags": [],
+    "file": "images/acb40edf070fc83f9b837c7db061795e6818e250.png"
+  }
+];
